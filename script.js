18968227fefecf6190dfd94fb3a322f053adb459
DarshanPatil1128 <86683077+DarshanPatil1128@users.noreply.github.com>
2022-07-24 23:52:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var username = document.querySelector("#username");
+
+function edit() {
+    username.innerText = "Marisa G";
+}
+
+var a = [
+    document.querySelector("#per"),
+    document.querySelector("#per1")
+]
+
+function hide(id)
+{ 
+    a[id].remove();
+}
+
